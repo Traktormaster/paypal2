cfa75ad8be038fe3d786ab8add72430ba38aca4e
Nándor Mátravölgyi <nandor.matra@gmail.com>
2024-08-04 15:10:35 +0200

    Bump version: 0.1.2 → 0.1.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from collections import defaultdict
 from setuptools import setup, find_packages
 
 HERE = os.path.abspath(os.path.dirname(__file__))
-VERSION = "0.1.2"
+VERSION = "0.1.3"
 
 with open(os.path.join(HERE, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
